198516937328ad664c8f5f242c3b346776c2668e
ilanzgx <ilan-_@hotmail.com>
2026-03-09 14:58:17 -0300

    feat: add first connection from vue frontend to rust backend

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const systemInfo = ref<any>(null);
+const cpuInfo = ref<any>(null);
+
+onMounted(async () => {
+  systemInfo.value = await invoke("get_system_info");
+  cpuInfo.value = await invoke("get_cpu_info");
+});
+</script>
 
 <template>
   <h1>SpecScan</h1>
-  <h2>Hello World!</h2>
+  <h2 v-if="systemInfo">{{ systemInfo.os_name }}</h2>
+  <h2 v-if="cpuInfo">
+    {{ cpuInfo.name }} - {{ cpuInfo.cores }} cores - {{ cpuInfo.frequency }} MHz
+  </h2>
+  <h2 v-else>Carregando...</h2>
 </template>
